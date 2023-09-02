de9a672ea45ecee904757705815f27643d45da8e
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-02 12:31:26 +0330

    Update task.py
    Added get_type method

--- task.py
+++ task.py
@@ -1,40 +1,42 @@
 #parent
-class Person():
+class person():
     def __init__(self,name,lastname):
         self.name=name
         self.lastname=lastname
 
-    def fullname(self):
+    def full_name(self):
         return self.name+' '+self.lastname
+    def get_type(self):
+        return type(self).__name__
     
 
 #child1
-class Student(Person):
+class student(person):
     def __init__(self,name,lastname,score):
         super().__init__(name,lastname)
         self.score=score
     
-    def stufullname(self):
-        return super().fullname()
+    def full_name(self):
+        return super().full_name()
 
 #child2
-class Teacher(Person):
+class teacher(person):
     def __init__(self,name,lastname,stu_obj):
         super().__init__(name,lastname)
         self.stu_obj=stu_obj
     
-    def tchfullname(self):
-        return super().fullname()
+    def full_name(self):
+        return super().full_name()
     
     def __str__(self):
         return f'Student: {self.stu_obj.name} {self.stu_obj.lastname}'
     
 #object 
-stu1=Student('Zahra','Ahmadi',19)
-tch1=Teacher('Ali','Akbari',stu1)
+stu1=student('Zahra','Ahmadi',19)
+tch1=teacher('Ali','Akbari',stu1)
 
 #call
-print(stu1.stufullname())
+print(stu1.full_name())
 print(tch1)
-
-    
+print(stu1.get_type())
+print(tch1.get_type())
