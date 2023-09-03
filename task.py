a0b3a00c8106321e4eb4203119622ce0843b4a16
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-03 08:48:25 +0330

    Update task.py
    task file was updated

--- task.py
+++ task.py
@@ -1,42 +1,23 @@
 #parent
-class person():
+class Person():
     def __init__(self,name,lastname):
         self.name=name
         self.lastname=lastname
-
+        self.full_name
+    @property
     def full_name(self):
         return self.name+' '+self.lastname
-    def get_type(self):
-        return type(self).__name__
     
-
+    def __str__(self):
+        return type(self).__name__+f': {self.name} {self.lastname}'
+    
 #child1
-class student(person):
+class Student(Person):
     def __init__(self,name,lastname,score):
         super().__init__(name,lastname)
         self.score=score
     
-    def full_name(self):
-        return super().full_name()
-
 #child2
-class teacher(person):
-    def __init__(self,name,lastname,stu_obj):
+class Teacher(Person):
+    def __init__(self,name,lastname):
         super().__init__(name,lastname)
-        self.stu_obj=stu_obj
-    
-    def full_name(self):
-        return super().full_name()
-    
-    def __str__(self):
-        return f'Student: {self.stu_obj.name} {self.stu_obj.lastname}'
-    
-#object 
-stu1=student('Zahra','Ahmadi',19)
-tch1=teacher('Ali','Akbari',stu1)
-
-#call
-print(stu1.full_name())
-print(tch1)
-print(stu1.get_type())
-print(tch1.get_type())
