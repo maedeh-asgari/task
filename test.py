c60e60af69487ccd4db7c0d2eab0569bbba35dfd
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-03 08:50:02 +0330

    Update test.py
    test file was updated

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
 import unittest
-from task import student,teacher,tch1
+from task import Person,Student,Teacher
 
 class test_teacher(unittest.TestCase):
-    def test_str(self):
-        result=str(tch1)
+
+    @classmethod
+    def setUpClass(self):
+        self.person=Person('Maedeh','Asgari')
+        self.student=Student('Zahra','Ahamadi',19)
+        self.teacher=Teacher('Ali','Miri')
+    
+    @classmethod
+    def tearDownClass(self):
+        del self.person
+        del self.student
+        del self.teacher
+
+    def test_person_str(self):
+        result=str(self.person)
+        self.assertIn('Person',result)
+
+    def test_student_str(self):
+        result=str(self.student)
         self.assertIn('Student',result)
 
+    def test_teacher_str(self):
+        result=str(self.teacher)
+        self.assertIn('Teacher',result)
+
+if __name__=='__main__':
+    unittest.main()
     
