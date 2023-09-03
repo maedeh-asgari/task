1fa0fb35b52dbcffbae3c0a6f02e5eb4ee2d60ad
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-03 10:46:56 +0330

    Update task.py
    The task file was modified

--- task.py
+++ task.py
@@ -11,6 +11,15 @@ class Person():
     def __str__(self):
         return type(self).__name__+f': {self.name} {self.lastname}'
     
+    def get_full_name_with_prefix(self):
+        if type(self).__name__=='Person':
+            return f'per: {self.name} {self.lastname}'
+        if type(self).__name__=='Student':
+            return f'stu: {self.name} {self.lastname}'
+        if type(self).__name__=='Teacher':
+            return f'tch: {self.name} {self.lastname}'
+
+    
 #child1
 class Student(Person):
     def __init__(self,name,lastname,score):
