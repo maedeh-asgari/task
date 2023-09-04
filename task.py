28d876daa7c82f7af0623201c006fbe6b5c51859
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-04 10:19:57 +0330

    Update task.py
    class variable added

--- task.py
+++ task.py
@@ -1,9 +1,11 @@
 #parent
 class Person():
+    prefix='per'
     def __init__(self,name,lastname):
         self.name=name
         self.lastname=lastname
         self.full_name
+
     @property
     def full_name(self):
         return self.name+' '+self.lastname
@@ -12,21 +14,17 @@ class Person():
         return type(self).__name__+f': {self.name} {self.lastname}'
     
     def get_full_name_with_prefix(self):
-        if type(self).__name__=='Person':
-            return f'per: {self.name} {self.lastname}'
-        if type(self).__name__=='Student':
-            return f'stu: {self.name} {self.lastname}'
-        if type(self).__name__=='Teacher':
-            return f'tch: {self.name} {self.lastname}'
+        return f'{self.prefix}: {self.name} {self.lastname}'
 
-    
 #child1
 class Student(Person):
+    prefix='stu'
     def __init__(self,name,lastname,score):
         super().__init__(name,lastname)
         self.score=score
     
 #child2
 class Teacher(Person):
+    prefix='tch'
     def __init__(self,name,lastname):
         super().__init__(name,lastname)
